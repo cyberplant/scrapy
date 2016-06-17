fde22876652dd929504bbe34e9a61552b5a98740
Luar Roji <luar@scrapinghub.com>
2016-06-17 01:44:03 -0300

    Reenable RPM

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-apt-get install ruby-dev gcc make
+apt-get install ruby-dev gcc make rpm
 gem install fpm
 
-#for i in deb rpm; do
-for i in deb; do
+for i in deb rpm; do
   fpm -s python -t $i -n scrapy .
 done
