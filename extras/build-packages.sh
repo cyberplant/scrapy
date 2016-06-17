80c242b861bab2dc3cfc628ba45402fdac478a49
Luar Roji <luar@scrapinghub.com>
2016-06-17 05:09:43 -0300

    Move everything to /tmp

--- extras/build-packages.sh
+++ extras/build-packages.sh
@@ -5,7 +5,11 @@ gem install fpm
 
 fpm --verbose --debug -f -s python -t deb -n scrapy .
 
+mv *deb /tmp
+
 set
 ls -l 
+ls -l /tmp
+
 # Disabled for now
 # fpm --verbose --debug -s python -t rpm -n scrapy .
